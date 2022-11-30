8ddce7f2ee7a1d9f80a3238dcc34e2421a3a5958
Satechmedia <88564875+Satechmedia@users.noreply.github.com>
2022-11-30 11:39:43 +0100

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,9 +10,9 @@ const basePromptPrefix = `
 Write me a detailed table of contents for a blog post with the title below.
 
 Title:
-  `;
+`;
+
 const generateAction = async (req, res) => {
-  // first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
   const baseCompletion = await openai.createCompletion({
@@ -24,26 +24,31 @@ const generateAction = async (req, res) => {
 
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  // second prompt
+  // I build Prompt #2.
   const secondPrompt = `
-    Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+  Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
 
-    Title: ${req.body.userInput}
+  Title: ${req.body.userInput}
 
-    Table of Contents: ${basePromptOutput.text}
+  Table of Contents: ${basePromptOutput.text}
 
-    Blog Post:
+  Blog Post:
   `;
 
+  // I call the OpenAI API a second time with Prompt #2
   const secondPromptCompletion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${secondPrompt}`,
-    temperature: 0.89,
-    max_tokens: 1900,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 1250,
   });
 
+  // Get the output
   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
 
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
   res.status(200).json({ output: secondPromptOutput });
 };
 
